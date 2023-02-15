f0816abc21c66efd5a29c84721a2efa961614585
DEDA <beebsdone@gmail.com>
2023-02-15 18:24:49 +0100

    trying to implement relayPool

--- public/relays.js
+++ public/relays.js
@@ -1,68 +1,35 @@
-//DEFAULT RELAYS
 var pubKey = localStorage.getItem("pubKey");
 var privKey = localStorage.getItem("privKey");
-var npub = localStorage.getItem("npub");
-var nsec = localStorage.getItem("nsec");
 
-relay1URL = "wss://nostr-pub.wellorder.net";
-relay2URL = "wss://relay.nostr.info"
+export const pool = new window.NostrTools.SimplePool();
 
-const defaultRelay1 = window.NostrTools.relayInit(relay1URL);
-const defaultRelay2 = window.NostrTools.relayInit(relay2URL);
+let relay1URL = "wss://nostr-pub.wellorder.net";
+let relay2URL = "wss://nostr-pub.wellorder.net";
 
-defaultRelay1.connect();
-defaultRelay2.connect();
+export let relays = ["wss://nostr-pub.wellorder.net", "wss://nostr-pub.wellorder.net"]
 
-defaultRelay1.on('connect', () => {
-console.log(`connected to ${defaultRelay1.url}`)
-})
-defaultRelay1.on('error', () => {
-console.log(`failed to connect to ${defaultRelay1.url}`)
+let relay = await pool.ensureRelay('wss://relay.damus.io')
+
+let subs = pool.sub([...relays, relay], {
+  authors: ['pubKey']
 })
 
-defaultRelay2.on('connect', () => {
-console.log(`connected to ${defaultRelay2.url}`)
-})
-defaultRelay2.on('error', () => {
-console.log(`failed to connect to ${defaultRelay2.url}`)
-})
+subs.forEach(sub =>
+  sub.on('event', event => {
+    console.log('got event:', event);
+  })
+)
 
-//SUB TO ME
-let subMe1kind1 = defaultRelay1.sub([
-    {
-        kinds: [1],
-        authors: [pubKey]
-    }
-])
+let events = await pool.list(relays, [{kinds: [0, 1]}]);
+console.log("Event 0 or 1: " + events);
 
-let subMe2kind1 = defaultRelay2.sub([
-    {
-        kinds: [1],
-        authors: [pubKey]
-    }
-])
 
-subMe1kind1.on('event', event => {
-console.log('Your Opinion: ', event)
-
-let content1 = event.content;
-console.log("Your Opinion: " + content1);
-
-})
-
-subMe2kind1.on('event', event => {
-console.log('Your Opinion: ', event)
-
-let content2 = event.content;
-console.log("Your Opinion: " + content2);
-
-})
-
-//GLOBAL CHAT
+/*GLOBAL CHAT
 let subGlobalRelay1 = defaultRelay1.sub([
     {
         kinds: [1],
         authors: []
     }
 ])
+*/
 
--- public/sendNote.js
+++ public/sendNote.js
@@ -1,35 +1,25 @@
+import { pool, relays } from './relays.js'
+
 function makeNote (note) {
-    let event = {
+    let newEvent = {
       kind: 1,
       pubkey: pubKey,
       created_at: Math.floor(Date.now() / 1000),
       tags: [],
       content: note
     };
-    event.id = window.NostrTools.getEventHash(event);
-    event.sig = window.NostrTools.signEvent(event, privKey);
+    newEvent.id = window.NostrTools.getEventHash(newEvent);
+    newEvent.sig = window.NostrTools.signEvent(newEvent, privKey);
 
-    let pubRelay1 = defaultRelay1.publish(event)
-    pubRelay1.on('ok', () => {
-      console.log(`${defaultRelay1.url} has accepted our event`)
-    })
-    pubRelay1.on('seen', () => {
-      console.log(`we saw the event on ${defaultRelay1.url}`)
-    })
-    pubRelay1.on('failed', reason => {
-      console.log(`failed to publish to ${defaultRelay1.url}: ${reason}`)
-    })
+	let pubs = pool.publish(relays, newEvent)
+	pubs.forEach(pub =>
+	  pub.on('ok', () => {
+	    console.log('relays accepted our event')
+	  })
+	)
 
-    let pubRelay2 = defaultRelay2.publish(event)
-    pubRelay1.on('ok', () => {
-      console.log(`${defaultRelay2.url} has accepted our event`)
-    })
-    pubRelay2.on('seen', () => {
-      console.log(`we saw the event on ${defaultRelay2.url}`)
-    })
-    pubRelay2.on('failed', reason => {
-      console.log(`failed to publish to ${defaultRelay2.url}: ${reason}`)
-    })
+	let relaysForEvent = pool.seenOn(newEvent.id);
+	console.log(relaysForEvent);
 
     modal.style.display = "none";
     document.getElementById('publish-textarea').value = '';
